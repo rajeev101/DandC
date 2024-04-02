ae42fd57e6969adf0f40772211d4ab35b3b23102
shivanikewat9691 <shivani.s@pixoatic.com>
2024-04-02 11:14:18 +0530

    add migrate

--- db/migrate/20240206164550_create_syllabuses.rb
+++ db/migrate/20240206164550_create_syllabuses.rb
@@ -1,6 +1,5 @@
 class CreateSyllabuses < ActiveRecord::Migration[6.1]
   def change
-    
     create_table :syllabuses do |t|
       t.references :course, null: false, foreign_key: true
       t.string :course_name
