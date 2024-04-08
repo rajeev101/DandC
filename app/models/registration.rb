871f5bf3ec98e8dd6cd742e28a8ef7ce8d843381
shivanikewat9691 <shivani.s@pixoatic.com>
2024-04-08 16:30:47 +0530

    showing message after submit

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,5 +1,4 @@
 class Registration < ApplicationRecord
-  self.table_name = :registrations
   validates :your_name, presence: true
   validates :email, presence: true
   validates :phone_number, presence: true
