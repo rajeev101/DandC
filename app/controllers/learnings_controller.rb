9f9ac0643951e413ec57bdce3677ad14265dae86
shivanikewat9691 <shivani.s@pixoatic.com>
2024-04-15 12:39:04 +0530

    fixed indentation

--- app/controllers/learnings_controller.rb
+++ app/controllers/learnings_controller.rb
@@ -1,5 +1,5 @@
 class LearningsController < ApplicationController
-   before_action :set_user, only:[:show]
+   # before_action :set_user, only:[:show]
   def index
     @courses = Course.all
     @faqs = Faq.all
@@ -34,33 +34,4 @@ class LearningsController < ApplicationController
     # @registrations = Registration.all
     @registration = Registration.new
   end
-
-
-
-  def create
-    registration = Registration.new(registration_params)
-
-    respond_to do |format|
-      if registration.save
-        SendMailer.send_email(registration.email, registration).deliver_now
-        format.html { redirect_to root_path, notice: 'Registration successful. Confirmation email sent.' }
-        format.json { render json: { success: true, message: 'Registration successful. Confirmation email sent.' } }
-      else
-        format.html { render :index }
-        format.json { render json: { success: false, errors: registration.errors.full_messages } }
-      end
-    end
-  end
-  def show
-
-  end
-
-  private
-  def set_user
-     @registration = Registration.find(params[:id])
-  end
-
-    def registration_params
-      params.require(:registration).permit(:your_name, :email, :phone_number, :course_of_interest, :message, :company_name, :college_name)
-    end
 end
